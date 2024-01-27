98e5432d80ccbf3ecb3ffe962891f0150942e1ec
Cem Yilmaz <cem.yilmaz@sap.com>
2024-01-27 22:20:47 +0100

    feat(web-app): setup package

---
+++ projects/web/app/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import './styles.css';
+
+export default function CustomApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>PeopleEat</title>
+            </Head>
+            <main className="app">
+                <Component {...pageProps} />
+            </main>
+        </>
+    );
+}
