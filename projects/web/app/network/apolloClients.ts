b06f8939f106215de9f085f85789f3965a6bd185
Cem Yilmaz <cem.yilmaz@sap.com>
2024-02-18 15:38:51 +0100

    feat(web-app): setup apollo client

---
+++ projects/web/app/network/apolloClients.ts
@@ -0,0 +1,64 @@
+import { ApolloClient, InMemoryCache, split, type NormalizedCacheObject } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createUploadLink } from 'apollo-upload-client';
+import { createClient } from 'graphql-ws';
+
+export function createComplexApolloClient(httpUri: string, wsUri: string): ApolloClient<NormalizedCacheObject> {
+    const httpLink = createUploadLink({ uri: httpUri, headers: { 'Apollo-Require-Preflight': 'true' }, credentials: 'include' });
+
+    const isBrowser = typeof window !== 'undefined';
+
+    if (!isBrowser) {
+        return new ApolloClient({
+            cache: new InMemoryCache(),
+            defaultOptions: {
+                watchQuery: {
+                    fetchPolicy: 'no-cache',
+                    errorPolicy: 'ignore',
+                },
+                query: {
+                    fetchPolicy: 'no-cache',
+                    errorPolicy: 'all',
+                },
+            },
+            link: httpLink,
+        });
+    }
+
+    const wsLink = new GraphQLWsLink(createClient({ url: wsUri }));
+
+    const splitLink = split(
+        ({ query }) => {
+            const definition = getMainDefinition(query);
+            return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+        },
+        wsLink,
+        httpLink,
+    );
+
+    return new ApolloClient({
+        cache: new InMemoryCache(),
+        defaultOptions: {
+            watchQuery: {
+                fetchPolicy: 'no-cache',
+                errorPolicy: 'ignore',
+            },
+            query: {
+                fetchPolicy: 'no-cache',
+                errorPolicy: 'all',
+            },
+        },
+        link: splitLink,
+    });
+}
+
+export function createApolloClient(cookieString?: string): ApolloClient<NormalizedCacheObject> {
+    return new ApolloClient({
+        uri: 'http://localhost:4000/graphql', // process.env.NEXT_PUBLIC_SERVER_URL,
+        credentials: 'include',
+        headers: { cookie: cookieString as string },
+        cache: new InMemoryCache(),
+        ssrMode: true,
+    });
+}
