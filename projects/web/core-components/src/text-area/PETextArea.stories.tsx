f1873fbcf410ba454a818aa0c63d5439a01ad3c1
Cem Yilmaz <cem.yilmaz@sap.com>
2024-02-29 20:29:13 +0100

    feat(web-core-components): text area

---
+++ projects/web/core-components/src/text-area/PETextArea.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PETextArea } from './PETextArea';
+
+const meta: Meta<typeof PETextArea> = {
+    component: PETextArea,
+    title: 'PETextArea',
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof PETextArea> = {
+    args: {
+        placeholder: 'Placeholder',
+        errorMessage: 'Error Message',
+        labelTitle: 'Label Title',
+    },
+};
