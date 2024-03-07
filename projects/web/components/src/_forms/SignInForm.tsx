88bf4688d56cce5cb7a1abac3a5e956a894bb5f4
Cem Yilmaz <cem.yilmaz@sap.com>
2024-03-07 21:55:09 +0100

    feat(web-components): sign in form

---
+++ projects/web/components/src/_forms/SignInForm.tsx
@@ -0,0 +1,54 @@
+import { PEButton, PETextField } from '@people-eat/web-core-components';
+import { useForm } from 'react-hook-form';
+
+export interface SignInFormInputs {
+    emailAddress: string;
+    password: string;
+}
+
+export interface SignInFormProps {
+    completeTitle: string;
+    onSignIn: (formData: SignInFormInputs) => void;
+    onSignUp: () => void;
+}
+
+export function SignInForm({ completeTitle, onSignIn, onSignUp }: SignInFormProps) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignInFormInputs>();
+
+    return (
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSignIn)}>
+                <PETextField
+                    id="email-address"
+                    labelTitle="E-Mail Adresse"
+                    type="email"
+                    autoComplete="email"
+                    errorMessage={errors.emailAddress?.message}
+                    {...register('emailAddress', { required: 'This field is required' })}
+                />
+
+                <PETextField
+                    id="password"
+                    labelTitle="Passwort"
+                    type="password"
+                    autoComplete="current-password"
+                    errorMessage={errors.emailAddress?.message}
+                    {...register('password', { required: 'This field is required' })}
+                />
+
+                <PEButton title={completeTitle} type="submit" />
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+                Du hast noch kein Profil?{' '}
+                <button className="font-semibold leading-6 text-orange-600 hover:text-orange-500" onClick={onSignUp}>
+                    Hier registrieren
+                </button>
+            </p>
+        </div>
+    );
+}
