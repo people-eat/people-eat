679f7eb6a33c00088c956ca70b23374dcc6d22ee
Cem Yilmaz <cem.yilmaz@sap.com>
2024-04-06 10:54:33 +0200

    introduces clarity and google analytics

--- projects/web/app/pages/_app.tsx
+++ projects/web/app/pages/_app.tsx
@@ -1,15 +1,33 @@
+import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
 import 'tailwindcss/tailwind.css';
-import './styles.css';
-import { ApolloProvider } from '@apollo/client';
 import { createComplexApolloClient } from '../network/apolloClients';
+import './styles.css';
 
 const apolloClient = createComplexApolloClient();
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
+    useEffect(() => clarity.init('l6qxwo5j7v'));
+
     return (
         <>
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-165LKCGVJJ`} />
+
+            <Script id="ga-script" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-165LKCGVJJ', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
             <Head>
                 <title>PeopleEat</title>
             </Head>
