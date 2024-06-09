057a5c2fd0f56ef8c808a397aa5ac4aa9945ff18
Cem Yilmaz <cem.yilmaz@sap.com>
2024-06-09 16:25:10 +0200

    gift card section for home page

--- projects/web/app/pages/index.tsx
+++ projects/web/app/pages/index.tsx
@@ -365,6 +365,31 @@ export default function HomePage({ signedInUser, searchParams }: ServerSideProps
                     </div>
                 </div>
 
+                <div className="mx-auto max-w-7xl py-10 sm:px-6 lg:px-8">
+                    <div className="relative isolate overflow-hidden bg-orange-200 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+                        <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                            <h2 className="text-3xl font-bold tracking-tight text-indigo-950 sm:text-4xl">
+                                Bereite deinen Lieben ein unvergessliches Geschenk
+                            </h2>
+                            <p className="mt-6 text-lg leading-8 text-indigo-950">
+                                Ein Privater Koch für zuhause ist die neue Art, besondere Momente zu feiern
+                            </p>
+                            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                                <PELink title="Gutschein kaufen" href="/gift-cards" />
+                            </div>
+                        </div>
+                        <div className="relative mt-16 h-80 lg:mt-8">
+                            <Image
+                                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                                alt=""
+                                width={1824}
+                                height={1080}
+                            />
+                        </div>
+                    </div>
+                </div>
+
                 <div className="overflow-hidden py-32">
                     <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8 overflow-hidden">
                         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
