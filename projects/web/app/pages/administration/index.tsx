ccd72842b8baf506cf20d522039a74d993f80bf9
Cem Yilmaz <cem.yilmaz@sap.com>
2024-10-15 22:22:11 +0200

    consume and display metrics

--- projects/web/app/pages/administration/index.tsx
+++ projects/web/app/pages/administration/index.tsx
@@ -1,28 +1,31 @@
 import { PEHeader } from '@people-eat/web-components';
-import { GetSignedInUserDocument, SignedInUser } from '@people-eat/web-domain';
+import { AdminGetPageDataDocument, AdminGetPageDataQuery, SignedInUser } from '@people-eat/web-domain';
 import { GetServerSideProps, Redirect } from 'next';
 import Link from 'next/link';
-import { createApolloClient } from '../../network/apolloClients';
+import { Chart } from 'react-google-charts';
 import { redirectTo } from '../../components/redirectTo';
+import { createApolloClient } from '../../network/apolloClients';
 
 const profilePageRedirect: { redirect: Redirect } = { redirect: { permanent: false, destination: '/profile' } };
 
 interface ServerSideProps {
     signedInUser: SignedInUser;
+    countMetrics: NonNullable<NonNullable<AdminGetPageDataQuery['sessions']['current']['user']>['admin']>['metrics']['count'];
 }
 
 export const getServerSideProps: GetServerSideProps<ServerSideProps> = async ({ req }) => {
     const apolloClient = createApolloClient(req.headers.cookie);
 
     try {
-        const userData = await apolloClient.query({ query: GetSignedInUserDocument });
-        const signedInUser = userData.data.sessions.current.user;
-        if (!signedInUser) return redirectTo.signIn({ returnTo: req.url });
-        if (!signedInUser.isAdmin) return profilePageRedirect;
+        const { data } = await apolloClient.query({ query: AdminGetPageDataDocument });
+        const user = data.sessions.current.user;
+        if (!user) return redirectTo.signIn({ returnTo: req.url });
+        if (!user.admin) return profilePageRedirect;
 
         return {
             props: {
-                signedInUser,
+                signedInUser: user,
+                countMetrics: user.admin.metrics.count,
             },
         };
     } catch (error) {
@@ -40,7 +43,7 @@ const tabs: { title: string; path: string }[] = [
     { title: 'Buchungsanfragen', path: '/administration/booking-requests' },
 ];
 
-export default function AdministrationPage({ signedInUser }: ServerSideProps) {
+export default function AdministrationPage({ signedInUser, countMetrics }: ServerSideProps) {
     return (
         <div>
             <PEHeader signedInUser={signedInUser} />
@@ -48,7 +51,7 @@ export default function AdministrationPage({ signedInUser }: ServerSideProps) {
             <div className="mx-auto max-w-[88rem] px-4 md:pb-8 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-bold mb-8">Administration</h1>
 
-                <ul className="flex gap-4 flex-wrap">
+                <ul className="flex gap-4 flex-wrap mb-8">
                     {tabs.map((tab) => (
                         <li key={tab.path}>
                             <Link
@@ -60,6 +63,77 @@ export default function AdministrationPage({ signedInUser }: ServerSideProps) {
                         </li>
                     ))}
                 </ul>
+
+                <Chart
+                    chartType="ColumnChart"
+                    width="100%"
+                    options={{
+                        title: 'Benutzer Erstellungen',
+                    }}
+                    data={[
+                        ['Element', 'Anzahl', { role: 'style' }],
+                        ['vor 4 Wochen', countMetrics.totalUserCreations4WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 3 Wochen', countMetrics.totalUserCreations3WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 2 Wochen', countMetrics.totalUserCreations2WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['letzte Woche', countMetrics.totalUserCreationsLastWeek, 'rgb(51, 102, 204)'],
+                    ]}
+                />
+                <Chart
+                    chartType="ColumnChart"
+                    width="100%"
+                    options={{
+                        title: 'Kunden Profil Erstellungen',
+                    }}
+                    data={[
+                        ['Element', 'Anzahl', { role: 'style' }],
+                        ['vor 4 Wochen', countMetrics.totalCustomerCreations4WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 3 Wochen', countMetrics.totalCustomerCreations3WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 2 Wochen', countMetrics.totalCustomerCreations2WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['letzte Woche', countMetrics.totalCustomerCreationsLastWeek, 'rgb(51, 102, 204)'],
+                    ]}
+                />
+                <Chart
+                    chartType="ColumnChart"
+                    width="100%"
+                    options={{
+                        title: 'Koch Profil Erstellungen',
+                    }}
+                    data={[
+                        ['Element', 'Anzahl', { role: 'style' }],
+                        ['vor 4 Wochen', countMetrics.totalCookCreations4WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 3 Wochen', countMetrics.totalCookCreations3WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 2 Wochen', countMetrics.totalCookCreations2WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['letzte Woche', countMetrics.totalCookCreationsLastWeek, 'rgb(51, 102, 204)'],
+                    ]}
+                />
+                <Chart
+                    chartType="ColumnChart"
+                    width="100%"
+                    options={{
+                        title: 'Suchanfragen',
+                    }}
+                    data={[
+                        ['Element', 'Anzahl', { role: 'style' }],
+                        ['vor 4 Wochen', countMetrics.totalSearchRequests4WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 3 Wochen', countMetrics.totalSearchRequests3WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 2 Wochen', countMetrics.totalSearchRequests2WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['letzte Woche', countMetrics.totalSearchRequestsLastWeek, 'rgb(51, 102, 204)'],
+                    ]}
+                />
+                <Chart
+                    chartType="ColumnChart"
+                    width="100%"
+                    options={{
+                        title: 'Session Erstellungen',
+                    }}
+                    data={[
+                        ['Element', 'Anzahl', { role: 'style' }],
+                        ['vor 4 Wochen', countMetrics.totalSessions4WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 3 Wochen', countMetrics.totalSessions3WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['vor 2 Wochen', countMetrics.totalSessions2WeeksAgo, 'rgb(51, 102, 204)'],
+                        ['letzte Woche', countMetrics.totalSessionsLastWeek, 'rgb(51, 102, 204)'],
+                    ]}
+                />
             </div>
         </div>
     );
